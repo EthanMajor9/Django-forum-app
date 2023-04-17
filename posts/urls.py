39c5707458c757c69c5867f045c206c2f86cd8db
TkMajor12 <ethanmajor9@gmail.com>
2023-04-17 18:58:21 -0400

    Added load posts button

--- posts/urls.py
+++ posts/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
 
     path('hello-world/',hello_world_view, name='hello-world'),
-    path('data/', load_post_data_view, name='posts-data')
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data')
 ]
