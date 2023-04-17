01382f13ea5f11214f34b35fe8c48e71e44a5b80
TkMajor12 <ethanmajor9@gmail.com>
2023-04-17 18:34:48 -0400

    Added spinner and post cards

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('hello world');
 
 const helloworldBox = document.getElementById('hello-world');
 const postsBox = document.getElementById('posts-box');
+const spinnerBox = document.getElementById('spinner-box');
 
 $.ajax({
     type: 'GET',
@@ -19,13 +20,32 @@ $.ajax({
     type: 'GET',
     url: '/data/',
     success: function(response){
+        console.log(response)
         const data = response.data;
-        console.log(data)
-        data.forEach(idx => {
-            postsBox.innerHTML += `
-                ${idx.title} - <b>${idx.body}</b><br>
-            `
-        });
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(idx => {
+                postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${idx.title}</h5>
+                            <p class="card-text">${idx.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }); 
+        }, 100);
     },
     error: function(error){
         console.log('error', error);
