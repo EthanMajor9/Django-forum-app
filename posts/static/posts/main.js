1a5f650d0c5dcfd5d08628de2f58b6b3c02e61e3
TkMajor12 <ethanmajor9@gmail.com>
2023-04-17 18:13:48 -0400

    Updated view for returning post content and added ajax logic to update webpage

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,33 @@
 console.log('hello world');
+
+const helloworldBox = document.getElementById('hello-world');
+const postsBox = document.getElementById('posts-box');
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success: ', response.text);
+        helloworldBox.textContent = response.text;
+    },
+    error: function(error){
+        console.log('error: ', error);
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        const data = response.data;
+        console.log(data)
+        data.forEach(idx => {
+            postsBox.innerHTML += `
+                ${idx.title} - <b>${idx.body}</b><br>
+            `
+        });
+    },
+    error: function(error){
+        console.log('error', error);
+    }
+})
